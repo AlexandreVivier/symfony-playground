c2a39cc05a661dbaef4c3a44943efcb8ca52c3e0
Krill <80386194+AlexandreVivier@users.noreply.github.com>
2024-08-14 15:51:23 +0200

    feature/back
    titles
    
    ISSUE1

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
+use App\Form\RecipeType;
 use App\Repository\RecipeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -24,8 +26,60 @@ class RecipeController extends AbstractController
     {
         $recipe = $repository->find($id);
         if (!$recipe || $recipe->getSlug() !== $slug) {
+            $this->addFlash('danger', 'Recette introuvable');
             return $this->redirectToRoute('recipe.index', [], Response::HTTP_MOVED_PERMANENTLY);
         }
         return $this->render('recipe/show.html.twig', ['recipe' => $recipe]);
+    }
+
+    #[Route('/recettes/{id}/edit', name: 'recipe.edit', requirements: ['id' => '\d+'])]
+    public function edit(Request $request, Recipe $recipe, RecipeRepository $repository, EntityManagerInterface $em): Response
+    {
+        if (!$recipe) {
+            $this->addFlash('danger', 'Recette introuvable');
+            return $this->redirectToRoute('recipe.index', [], Response::HTTP_MOVED_PERMANENTLY);
+        }
+
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request); // le handleRequest set par défaut tous les champs avec les nouvelles valeurs transmises
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'Recette modifiée avec succès');
+            return $this->redirectToRoute('recipe.show', ['id' => $recipe->getId(), 'slug' => $recipe->getSlug()]);
+        }
+        return $this->render('recipe/edit.html.twig', ['recipe' => $recipe, 'form' => $form->createView()]);
+    }
+
+    #[Route('/recettes/new', name: 'recipe.new')]
+    public function new(Request $request, RecipeRepository $repository, EntityManagerInterface $em): Response
+    {
+        $recipe = new Recipe();
+        $form = $this->createForm(RecipeType::class, $recipe);
+
+        // On set la date de création et update à maintenant :
+        $recipe->setCreatedAt(new \DateTimeImmutable());
+        $recipe->setUpdatedAt(new \DateTimeImmutable());
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // On prend le titre qu'on sépare par des - pour set le slug :
+            $slug = $recipe->getTitle();
+            // // Convertir en minuscules
+            $slug = strtolower($slug);
+            // // Supprimer les accents
+            $slug = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $slug);
+            // // Remplacer les caractères non désirés par des tirets
+            // TODO changer les caracs par des lettres
+            $slug = preg_replace('/[^a-z0-9]+/', '-', $slug);
+            // // Supprimer les tirets en début et fin de chaîne
+            $slug = trim($slug, '-');
+            $recipe->setSlug($slug);
+            // dd($slug);
+            $em->persist($recipe);
+            $em->flush();
+            $this->addFlash('success', 'Recette ajoutée avec succès');
+            return $this->redirectToRoute('recipe.show', ['id' => $recipe->getId(), 'slug' => $recipe->getSlug()]);
+        }
+        return $this->render('recipe/new.html.twig', ['form' => $form->createView()]);
     }
 }
---
+++ src/Form/RecipeType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Recipe;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class RecipeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+            ])
+            // ->add('slug', TextType::class, [
+            //     'label' => 'Slug',
+            // ])
+            ->add('content', TextType::class, [
+                'label' => 'Contenu',
+            ])
+            // ->add('createdAt', null, [
+            //     'widget' => 'single_text',
+            // ])
+            // ->add('updatedAt', null, [
+            //     'widget' => 'single_text',
+            // ])
+            ->add('Duration', TextType::class, [
+                'label' => 'Durée',
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Envoyer',
+                'attr' => ['class' => 'btn btn-success'],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Recipe::class,
+        ]);
+    }
+}
